6192359c06fb258dfcb6c3c55986b4c128a762e7
Halil İbrahim Çamur <180101028@ogrenci.yalova.edu.tr>
2022-04-23 16:20:31 +0300

    Add project files.

---
+++ YeniMediaMarkt/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using YeniMediaMarkt.ViewModels;
+
+namespace YeniMediaMarkt.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}

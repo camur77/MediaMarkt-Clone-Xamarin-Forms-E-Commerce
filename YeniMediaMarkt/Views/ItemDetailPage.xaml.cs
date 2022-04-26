af531ed91df7bb5c06412e3315a5f34b1ae51610
yavuzbilginoglu <yavuzbilginoglu@gmail.com>
2022-04-26 15:28:48 +0300

    favoriler sayfası eklendi

--- YeniMediaMarkt/Views/ItemDetailPage.xaml.cs
+++ YeniMediaMarkt/Views/ItemDetailPage.xaml.cs
@@ -1,6 +1,6 @@
-﻿using System.ComponentModel;
+﻿using YeniMediaMarkt.ViewModels;
+using System.ComponentModel;
 using Xamarin.Forms;
-using YeniMediaMarkt.ViewModels;
 
 namespace YeniMediaMarkt.Views
 {
